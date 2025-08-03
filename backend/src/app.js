2b24829d8469526d84b31083b58629732dbdaa22
aksh24-exe <itsakshat07sharma@gmail.com>
2025-08-03 12:49:56 +0530

    Created a Express Server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,19 @@
-console.log("Starting a new Project");
+import express from "express"
+
+const app = express()
+
+app.use("/test",(req, res) => {
+    res.send("Hello From the Server")
+})
+
+
+app.get("/home", (req, res) => {
+    res.json({
+        "msg":"Hello"
+    })
+})
+
+app.listen(8000, () => {
+    console.log("Server is running");
+    
+})
