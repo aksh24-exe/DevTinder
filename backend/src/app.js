481842d0b52cf99a04dad57a7ba18e483088ea22
aksh24-exe <itsakshat07sharma@gmail.com>
2025-08-09 14:36:03 +0530

    Error Handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,24 @@
 import express from "express"
-import { adminAuth, userCheck } from "./Middleware/auth.middleware.js";
 
 const app = express()
 
-app.use("/admin", adminAuth)
 
-app.get("/user",userCheck, (req, res) => {
-    res.status(201).send("It is valid user")
+
+app.get("/getUserData", (req, res) => {
+    // Logic of DB call and get user Data
+    // try {
+    //     res.status(404).send()
+    // } catch (error) {
+        
+    // }
+    throw new Error("dfg");
+
 } )
 
-app.get("/admin/getAllData", (req, res) => {
-    res.send("Get all the Data")
-})
-
-app.delete("/admin/deleteAllData", (req, res) => {
-    res.send("All users will we deleted")
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(401).send("Error")
+    }
 })
 
 app.listen(8000, () => {
